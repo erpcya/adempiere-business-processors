e74b1956a5cf9d9e30ae5feb4c12b65bba329fee
Carlos Parada <cparada@erpya.com>
2025-12-12 18:20:31 -0400

    fix references

--- src/main/java/org/spin/eca46/util/support/DKron.java
+++ src/main/java/org/spin/eca46/util/support/DKron.java
@@ -22,13 +22,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.MClient;
